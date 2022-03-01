89cfc576056e59379125b5f6d1771ad868cc7d23
Leonardo Moreira <leonardo.moreira@integer.pt>
2022-03-01 00:55:36 +0000

    git twoNumberSum

--- TwoNumberSum/index.js
+++ TwoNumberSum/index.js
@@ -1,9 +1,33 @@
 "use strict";
 exports.__esModule = true;
-exports.hello = void 0;
-function hello(world) {
-    if (world === void 0) { world = 'world'; }
-    console.log("Hello ".concat(world, "! "));
+exports.twoNumberSum = void 0;
+function twoNumberSum(array, targetSum) {
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array.length; j++) {
+            if (array[i] + array[j] === targetSum && array[i] !== array[j]) {
+                return [array[i], array[j]];
+            }
+        }
+    }
+    return [];
 }
-exports.hello = hello;
-hello();
+exports.twoNumberSum = twoNumberSum;
+// export function twoNumberSum(array: number[], targetSum: number) {
+// 	var nums: number[] = [];
+//     debugger
+// 	for (let num of nums) {
+// 		let potentialMatch = targetSum - num;
+// 		if (potentialMatch in nums)
+// 			return [potentialMatch, num];
+// 		else
+// 			nums[num] = 1;
+// 	}
+//   return [];
+// }
+var array = [3, 5, -4, 8, 11, 1, -1, 6];
+var targetSum = 10;
+console.log(twoNumberSum(array, targetSum));
+// export function hello(world: string = 'world'): void {
+//   console.log(`Hello ${world}! `);
+// }
+// hello();
--- TwoNumberSum/index.ts
+++ TwoNumberSum/index.ts
@@ -1,5 +1,39 @@
-export function hello(world: string = 'world'): void {
-  console.log(`Hello ${world}! `);
+export function twoNumberSum(array: number[], targetSum: number) {
+    for (let i: number = 0; i < array.length; i++) {
+        for (let j: number = 0; j < array.length; j++) {
+            if (array[i] + array[j] === targetSum && array[i] !== array[j]) {
+                return [array[i], array[j]];
+            }            
+        }
+    }
+    
+    return [];
 }
 
-hello();
+// export function twoNumberSum(array: number[], targetSum: number) {
+// 	var nums: number[] = [];
+//     debugger
+// 	for (let num of nums) {
+// 		let potentialMatch = targetSum - num;
+// 		if (potentialMatch in nums)
+// 			return [potentialMatch, num];
+// 		else
+// 			nums[num] = 1;
+// 	}
+	
+//   return [];
+// }
+
+
+let array = [3, 5, -4, 8, 11, 1, -1, 6];
+let targetSum = 10;
+
+console.log(twoNumberSum(array, targetSum));
+
+
+
+// export function hello(world: string = 'world'): void {
+//   console.log(`Hello ${world}! `);
+// }
+
+// hello();
