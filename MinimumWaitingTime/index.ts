85b63dd30681501869c4dbe9f2567600a719a106
Leonardo Moreira <leonardo.moreira@xpand-it.com>
2022-07-13 20:28:44 +0100

    MinimumWaitingTime

---
+++ MinimumWaitingTime/index.ts
@@ -0,0 +1,38 @@
+// Time = O(nlogn) [because we have to sort the list] | Space = O(1) [Dont need extra space]
+
+// export function minimumWaitingTime(queries: number[]) {
+//   queries.sort((a, b) => a - b);
+  
+//   let sum: number = 0;
+//   let total: number = 0;
+//   for (let i: number = 0; i < queries.length - 1; i++) {
+//     sum = sum + queries[i];
+//     total = total + sum;
+//   }
+  
+//   return total;
+// }
+
+
+export function minimumWaitingTime(queries: number[]) {
+  queries.sort((a, b) => a - b);
+  
+  let total: number = 0;
+  for (let i: number = 0; i < queries.length - 1; i++) {
+    console.log("(queries.length - i)", (queries.length - i))
+    console.log("queries[i] * (queries.length - i)", queries[i] * (queries.length - i))
+    total += queries[i] * (queries.length - (i+1));
+  }
+  
+  return total;
+}
+
+const queries = [3, 2, 1, 2, 6];
+
+console.log(minimumWaitingTime(queries));
+
+// Sort
+// 1 2 2 3 6
+
+// Sum
+// (0) + (1) + (1+2) + (3+2) + (5+3) = 1+3+5+8 ) 17
